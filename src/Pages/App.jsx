5dc4f575d9bf0587c21060786519ea30865360e4
Alexey Dubino <alexd2008add@gmail.com>
2024-05-13 22:02:16 +0300

    made picture books

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,26 +1,19 @@
 import "../Style/App.scss";
 import Account from "./Account";
 import HomeScreen from "../Components/HomeScreen/HomeScreen";
-import Genres from "../Components/Genres/Genres";
-import NavPanel from "../Components/NavPanel/NavPanel";
-import Prime from "../Components/Prime/Prime";
-import Hearing from "../Components/Hearing/Hearing";
-import Catalog from "../Components/Catalog/Catalog";
 
 
 import LogIn from "./LogIn/LogIn";
 import SignUp from "./SignUp/SignUp";
 
-import { useState, useCallback, useEffect } from 'react';
-import { getAuth } from 'firebase/auth';
-
+import { useState, useCallback, useEffect } from "react";
+import { getAuth } from "firebase/auth";
 
 import Payment from "../Components/Payment/Payment";
 
 function App() {
-
-  const [showLogIn , setShowLogIn] = useState(true)
-  const [showSignUp , setShowSignUp] = useState(false)
+  const [showLogIn, setShowLogIn] = useState(true);
+  const [showSignUp, setShowSignUp] = useState(false);
 
   const handleHideLogIn = useCallback(() => setShowLogIn(false), []);
   const handleHideSingUp = useCallback(() => setShowSignUp(false), []);
@@ -32,7 +25,7 @@ function App() {
   //     unscribe
   //     const {currentUser} = getAuth()
   //     if (!currentUser) {
-        
+
   //     }
   //   } )
   // })
@@ -40,21 +33,17 @@ function App() {
   return (
     <>
       {/* <Account /> */}
-      {/* <div className="container">
+      <div className="container">
         <HomeScreen />
-        <Genres />
-        <Prime />
-        <Catalog />
-        <Hearing />
-        <NavPanel />
-
+       
       </div>
-      {showLogIn && <LogIn hideLogIn={handleHideLogIn} showSignUp={handleShowSingUp}/>}
-      {showSignUp && <SignUp hideSignUp={handleHideSingUp} showLogIn={handleShowLogIn}/>}
-
-      </div> */}
-      <Payment/>
-
+      {/* {showLogIn && (
+        <LogIn hideLogIn={handleHideLogIn} showSignUp={handleShowSingUp} />
+      )}
+      {showSignUp && (
+        <SignUp hideSignUp={handleHideSingUp} showLogIn={handleShowLogIn} />
+      )} */}
+      {/* // <Payment/> */}
     </>
   );
 }
